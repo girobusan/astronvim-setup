0091de6c769e50a8c0babbc75c7eeb7732304216
Girobusan <private>
2024-07-09 23:24:54 +0300

    +surround

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -14,6 +14,16 @@ return {
     init = function() vim.g.mkdp_filetypes = { "markdown" } end,
     ft = { "markdown" },
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
   -- "andweeb/presence.nvim",
   {
     "ray-x/lsp_signature.nvim",
