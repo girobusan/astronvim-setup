b1bb5faad815f25b28ebe316d28d7f003bce4303
Gleb T <girobusan@>
2024-07-10 18:07:25 +0300

    +smartline

--- lua/community.lua
+++ lua/community.lua
@@ -9,5 +9,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
   -- import/override with your plugins folder
 }
