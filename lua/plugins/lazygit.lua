c1d67db9d2184efb9ce0aec7dab6cab46cbedeeb
Gleb T <girobusan@>
2024-07-11 19:46:58 +0300

    Removed excess mapping

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -15,8 +15,8 @@ return {
     },
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>gz", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    },
+    -- keys = {
+    --   { "<leader>gz", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    -- },
   },
 }
