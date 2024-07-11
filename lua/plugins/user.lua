61888dc548e67071eb479d70ac65e9a9d195ce04
Gleb T <girobusan@>
2024-07-11 19:37:31 +0300

    Clean up

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,7 +1,8 @@
 -- You can also add or configure plugins by creating files in this `plugins/` folder
--- Here are some examples:
+
 local get_icon = require("astroui").get_icon
 
+-- Here are some examples:
 ---@type LazySpec
 return {
 
@@ -58,20 +59,8 @@ return {
     opts = function(_, opts)
       opts.filesystem.filtered_items.visible = true
       opts.filesystem.filtered_items.hide_dotfiles = false
+      opts.filesystem.filtered_items.hide_gitignored = true
     end,
-    --   config = function()
-    --     require("neo-tree").setup {
-    --       filesystem = {
-    --         filtered_items = {
-    --           visible = true,
-    --           hide_dotfiles = false,
-    --           hide_gitignored = false,
-    --         },
-    --       },
-    --       source_selector = {
-    --         winbar = true,
-    --         statusline = false,
-    --       },
     --       reveal_fsync_root_with_cwd = true,
     --       respect_buf_cwd = true,
     --       update_focused_file = {
@@ -81,12 +70,6 @@ return {
     --       },
     --     }
     --   end,
-    --   dependencies = {
-    --     "nvim-lua/plenary.nvim",
-    --     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    --     "MunifTanjim/nui.nvim",
-    --     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    --   },
   },
   -- customize alpha options
   {
