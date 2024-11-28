52c7c0fde3059a765c1dcff971d924ef16fea057
Girobusan <private>
2024-11-28 11:29:34 +0300

    + rainbow csv

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -66,6 +66,25 @@ return {
       }
     end,
   },
+  {
+    "cameron-wags/rainbow_csv.nvim",
+    config = true,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+    cmd = {
+      "RainbowDelim",
+      "RainbowDelimSimple",
+      "RainbowDelimQuoted",
+      "RainbowMultiDelim",
+    },
+  },
 
   {
     "cryptomilk/nightcity.nvim",
