383bb6ab362c176761e96a96e8947a07a5cb9bc5
Girobusan <private>
2026-01-14 18:26:13 +0300

    fixes

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -43,14 +43,14 @@ return {
       -- configuration here or empty for defaults
     },
   },
-  {
-    "MeanderingProgrammer/markdown.nvim",
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-    config = function() require("render-markdown").setup {} end,
-  },
+  -- {
+  --   "MeanderingProgrammer/markdown.nvim",
+  --   name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  --   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+  --   config = function() require("render-markdown").setup {} end,
+  -- },
   {
     "danymat/neogen",
     config = true,
@@ -105,6 +105,15 @@ return {
   },
 
   -- == Examples of Overriding Plugins ==
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = function(_, opts)
@@ -185,6 +194,9 @@ return {
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
     end,
+  },
+  {
+    "mpas/marp-nvim",
   },
 
   {
